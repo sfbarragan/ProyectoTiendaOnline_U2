b5498ff523fee497543f8519f756db8f7aacf111
gbheredia22Z <gbheredia@espe.edu.ec>
2022-07-18 07:34:26 -0500

    AccessController

---
+++ ProyectoTiendaOnline_U2/Controllers/AccessController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoTiendaOnline_U2.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
