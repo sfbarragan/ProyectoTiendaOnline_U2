cb4f2e48ba69f8c903c04b68085fd34722de10e0
Celucio <75482093+Celucio@users.noreply.github.com>
2022-07-23 21:35:50 -0500

    Cambios realizados

---
+++ ProyectoTiendaOnline_U2/Models/ViewModel/ListMetodosViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTiendaOnline_U2.Models.ViewModel
+{
+    public class ListMetodosViewModel
+    {
+        public int id_modopago { get; set; }
+        [Required]
+        [Display(Name="Nombre Método")]
+        public string nombre { get; set; }
+
+    }
+}
---
+++ ProyectoTiendaOnline_U2/Models/ViewModel/MetodosViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTiendaOnline_U2.Models.ViewModel
+{
+    public class MetodosViewModel
+    {
+        public int id_modopago { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
